dceaa881dd969c8117eb696745a13d9d42087da8
petrukha-ivan <petrukha@macpaw.com>
2026-04-04 23:42:12 +0200

    Move tokenizer info builder

--- Sources/MLXStructured/GrammarMatcherFactory.swift
+++ Sources/MLXStructured/GrammarMatcherFactory.swift
@@ -13,114 +13,23 @@ extension GrammarMaskedLogitProcessor {
     private static let cache = Cache<ModelConfiguration, GrammarCompiler>()
 
     public static func from(
-        hub: HubApi = .shared,  // TODO: Request changes in swift-transformers to make the tokenizer vocab (and some other properties) public
+        hub: HubApi = .shared,
         configuration: ModelConfiguration,
         grammar: Grammar
     ) async throws -> GrammarMaskedLogitProcessor {
-        let compiler = try await grammarCompiler(for: configuration, hub: hub)
+        let compiler: GrammarCompiler
+        if let cached = await cache.value(for: configuration) {
+            compiler = cached
+        } else {
+            let tokenizerInfo = try await TokenizerInfo.from(hub: hub, configuration: configuration)
+            compiler = try GrammarCompiler(tokenizerInfo: tokenizerInfo)
+            await cache.set(compiler, for: configuration)
+        }
+
         let compiledGrammar = try compiler.compile(grammar: grammar)
         let grammarMatcher = try XGrammar(compiledGrammar: compiledGrammar)
         let processor = GrammarMaskedLogitProcessor(grammarMatcher: grammarMatcher)
         return processor
-    }
-
-    private static func grammarCompiler(
-        for configuration: ModelConfiguration,
-        hub: HubApi
-    ) async throws -> GrammarCompiler {
-        if let cached = await cache.value(for: configuration) {
-            return cached
-        }
-
-        let configurations =
-            switch configuration.id {
-            case .id(let id, let revision):
-                LanguageModelConfigurationFromHub(modelName: id, revision: revision, hubApi: hub)
-            case .directory(let directory):
-                LanguageModelConfigurationFromHub(modelFolder: directory, hubApi: hub)
-            }
-
-        let (modelConfig, tokenizerConfig, tokenizerData) = try await (
-            configurations.modelConfig,
-            configurations.tokenizerConfig,
-            configurations.tokenizerData
-        )
-
-        let modelVocab: [(token: String, id: Int)] = tokenizerData
-            .model.vocab.dictionary(or: [:])
-            .compactMap { key, value in
-                if let id = value.integer() {
-                    return (token: key.string, id: id)
-                } else {
-                    return nil
-                }
-            }
-
-        let addedTokens: [(token: String, id: Int)] = tokenizerData
-            .addedTokens.array(or: [])
-            .compactMap { value in
-                if let id = value.id.integer(), let token = value.content.string() {
-                    return (token: token, id: id)
-                } else {
-                    return nil
-                }
-            }
-
-        let configuredVocabSize =
-            [
-                modelConfig?.vocabSize.integer(),
-                modelConfig?.textConfig.vocabSize.integer(),
-                modelConfig?.textConfiguration.vocabSize.integer(),
-            ]
-            .compactMap { $0 }
-            .max() ?? 0
-
-        let derivedVocabSize =
-            [
-                modelVocab.map(\.id).max(),
-                addedTokens.map(\.id).max(),
-            ]
-            .compactMap { $0 }
-            .map { $0 + 1 }
-            .max() ?? 0
-
-        let vocabSize = max(configuredVocabSize, derivedVocabSize)
-        var vocab = Array(repeating: "", count: vocabSize)
-        for (token, id) in (modelVocab + addedTokens) where vocab.indices.contains(id) {
-            vocab[id] = token
-        }
-
-        let decoders: [Config] =
-            switch tokenizerData.decoder.type.string() {
-            case "Sequence":
-                tokenizerData.decoder.decoders.array(or: [])
-            default:
-                [tokenizerData.decoder]
-            }
-
-        var vocabType: Int32 = 0
-        loop: for decoder in decoders {
-            switch decoder.type.string() {
-            case "ByteFallback":
-                vocabType = 1
-                break loop
-            case "ByteLevel":
-                vocabType = 2
-                break loop
-            default:
-                continue
-            }
-        }
-
-        var stopTokenIds: [Int32] = configuration.extraEOSTokens.compactMap(vocab.firstIndex).map(Int32.init)
-        if let tokenizerConfig, let eosToken = tokenizerConfig.eosToken.string(), let eosTokenId = vocab.firstIndex(of: eosToken) {
-            stopTokenIds.append(Int32(eosTokenId))
-        }
-
-        let tokenizerInfo = TokenizerInfo(vocab: vocab, vocabType: vocabType, stopTokenIds: stopTokenIds)
-        let compiler = try GrammarCompiler(tokenizerInfo: tokenizerInfo)
-        await cache.set(compiler, for: configuration)
-        return compiler
     }
 }
 
